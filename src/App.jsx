60c9b00d1974b85f1b9ea5280978496fae676544
UnderVarela <under.varela@gmail.com>
2023-06-08 16:16:14 +0200

    Añadido Fetch v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
 import { CharacterForm } from './components/CharacterForm'
 import { CharacterList } from './components/CharacterList'
+import { v4 as uuidv4 } from 'uuid'
 
 function App () {
   const [users, setUsers] = useState([])
+  fetch('https://swapi.dev/api/people')
+    .then(response => response.json())
+    .then(data => {
+      const { results } = data
+      const data2 = results.map(({ name, mass, height }) => {
+        return {
+          id: uuidv4(),
+          name,
+          mass,
+          height
+        }
+      })
+      setUsers(data2)
+    })
   function handleCharacter (payload) {
     setUsers([
       payload,
